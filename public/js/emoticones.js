ae24e063dd074ab45276729072476b604d009539
AlexisMetton <alexis.metton@gmail.com>
2022-08-30 16:56:16 +0200

    smiley

--- public/js/emoticones.js
+++ public/js/emoticones.js
@@ -1,7 +1,43 @@
-function emoticones(){
+function emoticones() {
     document.getElementById('emoticones').style.display = "flex";
 }
-function sourire(){
-    document.getElementById('envoyer_message').innerHTML += " &#x1F600; ";
+function sourire() {
+    document.getElementById('envoyer_message').value += " &#x1F600; ";
+    document.getElementById('emoticones').style.display = "none";
+}
+function auréole() {
+    document.getElementById('envoyer_message').value += " &#x1F607; ";
+    document.getElementById('emoticones').style.display = "none";
+}
+function bisous() {
+    document.getElementById('envoyer_message').value += " &#x1F618; ";
+    document.getElementById('emoticones').style.display = "none";
+}
+function clinoeil() {
+    document.getElementById('envoyer_message').value += " &#x1F609; ";
+    document.getElementById('emoticones').style.display = "none";
+}
+function coeur() {
+    document.getElementById('envoyer_message').value += " &#x1F970; ";
+    document.getElementById('emoticones').style.display = "none";
+}
+function démon() {
+    document.getElementById('envoyer_message').value += " &#x1F608; ";
+    document.getElementById('emoticones').style.display = "none";
+}
+function joie() {
+    document.getElementById('envoyer_message').value += " &#x1F602; ";
+    document.getElementById('emoticones').style.display = "none";
+}
+function lunnette() {
+    document.getElementById('envoyer_message').value += " &#x1F60E; ";
+    document.getElementById('emoticones').style.display = "none";
+}
+function peur() {
+    document.getElementById('envoyer_message').value += " &#x1F631; ";
+    document.getElementById('emoticones').style.display = "none";
+}
+function transpiration() {
+    document.getElementById('envoyer_message').value += " &#x1F605; ";
     document.getElementById('emoticones').style.display = "none";
 }
