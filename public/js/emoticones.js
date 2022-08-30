173342fcd7ef7acf0cd541fdd7f3ea82480ea8a3
AlexisMetton <alexis.metton@gmail.com>
2022-08-30 16:03:21 +0200

    debut emoticone + supp image inutiles

---
+++ public/js/emoticones.js
@@ -0,0 +1,7 @@
+function emoticones(){
+    document.getElementById('emoticones').style.display = "flex";
+}
+function sourire(){
+    document.getElementById('envoyer_message').innerHTML += " &#x1F600; ";
+    document.getElementById('emoticones').style.display = "none";
+}
